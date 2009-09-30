0c624f6c9752a341fa349d96195a49338c09030b
Steven Baker <steven@stevenrbaker.com>
2009-09-30 13:22:05 -0300

    update to match flog 2.2.0

--- lib/castigate/abuse/flog.rb
+++ lib/castigate/abuse/flog.rb
@@ -4,21 +4,20 @@ module Castigate
   module Abuse
     class Flog
       def self.columns
-        %w(average max size stddev total)
+        %w(average max size total)
       end
 
       def abuse commit
         files   = Dir["{app,bin,lib,spec,test}/**/*.rb"]
         flogger = ::Flog.new
 
-        flogger.flog_files files
+        flogger.flog files
         methods = flogger.totals.reject { |k,v| k =~ /\#none$/ }
 
         {
           :average => flogger.average,
           :max     => methods.values.max,
           :size    => methods.size,
-          :stddev  => flogger.stddev,
           :total   => flogger.total
         }
       rescue SyntaxError
