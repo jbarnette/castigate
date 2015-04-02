0d46bc5396f3b2f6b95f56c1f65ace8c0015efeb
Ryan Davis <ryand-ruby@zenspider.com>
2015-04-01 18:03:23 -0700

    Revamped Repo.abuse to have cached values.
    It efficiently skips needles checkouts for any cached values.
    
    The entire cache is read/written each run. This could get prohibitive on
    big projects with lots of abusers.

--- lib/castigate/repo.rb
+++ lib/castigate/repo.rb
@@ -1,4 +1,27 @@
 module Castigate
+  class Cache
+    def self.cache path, data = {}
+      data = read(path) if File.exist? path
+      yield data
+      data
+    ensure
+      write path, data
+    end
+
+    def self.read path
+      File.open path do |f|
+        Marshal.load f
+      end
+    end
+
+    def self.write path, data
+      File.open(path, "w") do |f|
+        data.default_proc = nil if Hash === data
+        Marshal.dump data, f
+      end
+    end
+  end
+
   class Repo
     attr_reader :dir
 
@@ -21,28 +44,35 @@ module Castigate
     end
 
     def abuse dest = nil
-      metrics = Hash.new { |h, k| h[k] = {} }
-      # FIXME: load existing metrics
+      cache_name = "#{File.basename(@dir)}.cache"
 
-      warn "Abusing #@dir with #{Abuse.constants.join(', ')}." if $DEBUG
+      Cache.cache cache_name do |metrics|
+        metrics.default_proc = lambda { |h, k| h[k] = {} } # hack?
 
-      each_commit do |commit|
-        metrics[:commit][commit.id] ||= commit.to_h
+        warn "Abusing #@dir with #{Abuse.constants.join(', ')}." if $DEBUG
 
-        Abuse.constants.each do |c|
-          klass  = Abuse.const_get(c)
-          key    = c.downcase.to_sym
-          abuser = klass.new
+        each_commit do |commit|
+          metrics[:commit][commit.id] ||= commit.to_h
 
-          # FIXME: only if the abuser hasn't seen this commit
-          metrics[key][commit.id] = abuser.abuse(commit)
+          Abuse.constants.each do |c|
+            klass  = Abuse.const_get(c)
+            key    = c.downcase.to_sym
+
+            next if metrics[key][commit.id]
+
+            abuser = klass.new
+
+            checkout commit
+            metrics[key][commit.id] ||= abuser.abuse(commit)
+          end
+
+          $stderr.printf "." if $DEBUG
         end
 
-        $stderr.printf "." if $DEBUG
-      end
+        warn "" if $DEBUG
 
-      warn "" if $DEBUG
-      metrics
+        metrics
+      end
     end
   end
 end
